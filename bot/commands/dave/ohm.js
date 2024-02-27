59bb313a5d654dae73e5ed75264813fb0b3aec6e
Steve <stevetuozzo@gmail.com>
2024-02-27 18:26:27 -0500

    Added /lame aka The Hall of Lame Added /ohm because some people need to chill out

---
+++ bot/commands/dave/ohm.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ohm")
+    .setDescription("🙏 Who's not being zen right now? 🧘")
+    .addUserOption(option =>
+        option.setName('user')
+        .setDescription('The offending party.')
+        .setRequired(true)),
+  async execute(interaction) {
+      const ohmEmbed = new EmbedBuilder()
+        .setTitle('🙏 OOOOOOOHHHHHHHMMMMMMM 🧘')
+        .setDescription(`🧊😎 <@${interaction.user.id}> wants <@${interaction.options.getUser('user').id}> to chill out! 🍹⛱️`)
+
+      let msg = await interaction.reply({ embeds: [ohmEmbed] });
+      msg = await interaction.fetchReply();
+
+      msg.react("👍");
+      msg.react("👎");
+  },
+};

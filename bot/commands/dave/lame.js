904f0ec91e33a46831a9b4acc817834fc11317b3
Steve <stevetuozzo@gmail.com>
2024-02-27 19:08:50 -0500

    Added to The Hall of Lame

--- bot/commands/dave/lame.js
+++ bot/commands/dave/lame.js
@@ -14,6 +14,7 @@ module.exports = {
           { name: 'Turan "The Racist" Turkishname', value: 'Untimely "jokes" lead to a timely death.' },
           { name: 'Michael "Spike" Valenzoola', value: 'Although they were warned, it was a tranny that sealed his fate.' },
           { name: 'Dean "Tech Support" Pektas and Julia "SAVAGE" Montesdoca', value: 'Lies, deceit, gaslighting, and the ambulance incident was their claim to lame.' },
+          { name: 'Owen "Glass Bones"', value: 'Suffering from paper skin and glass bones, too easy to troll, and so death took its toll.' },
           { name: '❗ Vistor Notice ❗', value: 'In an effort to preserve the health of "The Cord",\nsome ex-friends have not been added to the Hall of Lame.\nThankyou, please enjoy your visit.'}
         )
 

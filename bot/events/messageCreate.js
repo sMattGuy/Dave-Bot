e47b937b8233999c20a3129698be16c8fd4a34b5
Matthew Flammia <matthewfrankflammia@gmail.com>
2024-06-18 09:12:54 -0400

    Fixed formatting on poems

--- bot/events/messageCreate.js
+++ bot/events/messageCreate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, italic, bold, underline } = require('discord.js');
 const { get_wiki } = require('../helper/get_wiki.js');
 const globals = require('../helper/global_variables.js');
 const { Replies } = require('../DB/functions/dbObjects.js');
@@ -58,33 +58,35 @@ async function send_poem(message) {
             const poem_json = await response.json();
             const title = poem_json[0].title;
             let fullpoem = "";
-			let new_line_count = 0;
-			while(true){
-				if(new_line_count >= poem_json[0].lines.length){
-					// end of poem reached
-					break;
-				}
-				else if(new_line_count >= 15){
-					//break if getting too long
-					break;
-				}
-				else if(new_line_count >= 5 && poem_json[0].lines[new_line_count] == ""){
-					// attempt to get to at least the end of the paragraph
-					break;
-				}
-				else{
-					// add line to poem
-					if(poem_json[0].lines[new_line_count] != ""){
-						fullpoem += `*${poem_json[0].lines[new_line_count]}*\n`;
-					}
-					new_line_count++;
-				}
-			}
-            const final_poem = `**${title}**\n__By David__\n${fullpoem}`;
+			      let new_line_count = 0;
+			      while(true){
+				      if(new_line_count >= poem_json[0].lines.length){
+					      // end of poem reached
+      					break;
+			      	}
+              else if(new_line_count >= 15){
+                //break if getting too long
+                break;
+              }
+              else if(new_line_count >= 5 && poem_json[0].lines[new_line_count] == ""){
+                // attempt to get to at least the end of the paragraph
+                break;
+              }
+              else{
+                // add line to poem
+                if(poem_json[0].lines[new_line_count] != ""){
+                  fullpoem += italic(poem_json[0].lines[new_line_count])+"\n";
+                }
+                new_line_count++;
+              }
+		      	}
+            const poem_title = bold(title);
+            const dave_author = underline("By David");
+            const final_poem = `${poem_title}\n${dave_author}\n${fullpoem}`;
             message.channel.send(final_poem);
         } catch (err) {
-			console.log(error)
-            message.channel.send(`Nevermind...`);
+			    console.log(error)
+          message.channel.send(`Nevermind...`);
         }
     }
 }

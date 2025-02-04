0802174e2e6779aa79b3e702ee400282bdb3eb5d
Matthew Flammia <matthewfrankflammia@gmail.com>
2025-02-04 09:07:56 -0500

    Fixed issue with daves time showing.
    The Dave command will now correctly show how long he has been here for

--- bot/commands/dave/dave.js
+++ bot/commands/dave/dave.js
@@ -10,20 +10,20 @@ module.exports = {
 		const discordJoinDate = 1615680000000;
     const total_weeks = Math.floor((new Date() - discordJoinDate) / 604800000) - uncountedWeeks;
     const years = Math.floor(total_weeks/52);
-    const weeks = total_weeks - (total_weeks * years);
+    const weeks = total_weeks - (52 * years);
     const year_word = (years == 1) ? "Year" : "Years";
     const week_word = (weeks == 1) ? "Week" : "Weeks";
     const daveEmbed = new EmbedBuilder()
+    if(weeks == 0){
+      daveEmbed.setTitle(`⏱ ~${years} ${year_word}~ ⏱`)
+    }
+    if(years == 0){
+      daveEmbed.setTitle(`⏱ ~${weeks} ${week_word}~ ⏱`)  
+    }
     if(years == 0 && weeks == 0){
       daveEmbed.setTitle(`⏱ ~He just got here!~ ⏱`)
     }
-    else if(years == 0){
-      daveEmbed.setTitle(`⏱ ~${weeks} ${week_word}~ ⏱`)  
-    }
-    else if(weeks == 0){
-      daveEmbed.setTitle(`⏱ ~${years} ${year_word}~ ⏱`)
-    }
-    else{
+    if(years != 0 && weeks != 0){
       daveEmbed.setTitle(`⏱ ~${years} ${year_word}, ${weeks} ${week_word}~ ⏱`)
     }
 		let msg = await interaction.reply({ embeds: [daveEmbed] });

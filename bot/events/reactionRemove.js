f40288bd51171a074f00672b10413e9adefde5e9
Matthew Flammia <matthewfrankflammia@gmail.com>
2025-02-26 19:46:17 -0500

    Added upvote and downvote system
    Users can now use a special Dave emote to upvote or downvote messages
    and give the user karma for it

--- bot/events/reactionRemove.js
+++ bot/events/reactionRemove.js
@@ -1,5 +1,8 @@
 const { Events } = require('discord.js');
-const { Fortunes } = require('../DB/functions/dbObjects.js')
+const { Users, Fortunes } = require('../DB/functions/dbObjects.js')
+
+const upvote_id = 1344451256449765399;
+const downvote_id = 1344451255325687908; 
 
 module.exports = {
 	name: Events.MessageReactionRemove,
@@ -28,6 +31,22 @@ module.exports = {
           fortune_found.save();
         }
       } 
+    }
+    if(reaction.message.author.id != process.env.CLIENTID){
+      const reacted_users = await reaction.users.fetch()
+      if(reaction.emoji.id == upvote_id){
+        const user = await Users.findOne({where:{user_id: reaction.message.author.id}});
+        user.karma -= 1;
+        user.save();
+      }
+      else if(reaction.emoji.id == downvote_id){
+        if(reacted_users.has(reaction.message.author.id)){
+          return;
+        }
+        const user = await Users.findOne({where:{user_id: reaction.message.author.id}});
+        user.karma += 1;
+        user.save();
+      }
     }
 	},
 };

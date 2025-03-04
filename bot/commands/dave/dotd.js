e7ff37c00db2d887f90cfa50a5290e0aa7e65b0a
Matthew Flammia <matthewfrankflammia@gmail.com>
2025-03-04 09:51:13 -0500

    Added time punishment to dotd if taking penalty

--- bot/commands/dave/dotd.js
+++ bot/commands/dave/dotd.js
@@ -65,6 +65,7 @@ module.exports = {
 		      user = await Users.findOne({where:{user_id: interaction.user.id}});
           user.karma_penalty = karma_penalty;
           user.karma += karma_penalty;
+          user.last_fortune += Math.floor(penalty_time/2);
           await user.save();
           await displayDOTD(true);
           await generateTicket();
